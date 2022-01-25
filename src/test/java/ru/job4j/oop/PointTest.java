a820141dd848d432db74e8d160840b178c055e10
Tsypa90 <URSA2004@mail.ru>
2022-01-25 23:01:26 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643 #255050]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -2,38 +2,49 @@ package ru.job4j.oop;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
 
-public class PointTest extends TestCase {
+import static junit.framework.Assert.assertEquals;
 
-    public void testDistanceExp2() {
+public class PointTest {
+    @Test
+    public void testDistanceWhen00To02Then2() {
         Point a = new Point(0, 0);
         Point b = new Point(0, 2);
         double rsl = a.distance(b);
         double expected = 2;
         assertEquals(rsl, expected);
     }
-
-    public void testDistanceExp4() {
+    @Test
+    public void testDistanceWhen24To28Then4() {
         Point a = new Point(2, 4);
         Point b = new Point(2, 8);
         double rsl = a.distance(b);
         double expected = 4;
         assertEquals(rsl, expected);
     }
-
-    public void testDistanceExp3() {
+    @Test
+    public void testDistanceWhen21To52Then3() {
         Point a = new Point(2, 1);
         Point b = new Point(5, 2);
         double rsl = a.distance(b);
         double expected = 3.16;
         assertEquals(rsl, expected, 0.01);
     }
-
-    public void testDistance3d() {
+    @Test
+    public void testDistanceWhen000To122Then3() {
         Point a = new Point(0, 0, 0);
         Point b = new Point(1, 2, 2);
         double rsl = a.distance3d(b);
         double expected = 3;
         assertEquals(rsl, expected);
+    }
+    @Test
+    public void testDistanceWhen215To525Then3() {
+        Point a = new Point(2, 1, 5);
+        Point b = new Point(5, 2, 5);
+        double rsl = a.distance3d(b);
+        double expected = 3.16;
+        assertEquals(rsl, expected, 0.01);
     }
 }
