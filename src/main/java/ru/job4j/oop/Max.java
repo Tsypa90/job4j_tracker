f710febdfac22092a3e3b95068ae670cad558038
Tsypa90 <URSA2004@mail.ru>
2022-01-25 22:48:04 +0300

    3. Перегрузить метод max для трех чисел [#122644 #254991]

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -2,10 +2,7 @@ package ru.job4j.oop;
 
 public class Max {
     public static int max(int first, int second) {
-        boolean max = first >= second;
-        int result = max ? first : second;
-        System.out.println(result);
-        return result;
+        return first >= second ? first : second;
     }
 
     public static int max(int first, int second, int third) {
