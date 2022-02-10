c5402f79e768ab8fc7f10fa57ae53de1be2f6783
Tsypa90 <URSA2004@mail.ru>
2022-02-10 21:13:16 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #262509]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -43,5 +43,6 @@ public class StartUITest {
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         assertNull(deleted);
+
     }
 }
