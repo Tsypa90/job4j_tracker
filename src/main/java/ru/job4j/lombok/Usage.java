b28d5afaf42c2fcef3705809ceb1122bad7749e4
Tsypa90 <URSA2004@mail.ru>
2022-08-09 21:53:42 +0300

    Revert "3. @Builder [#504890]"
    This reverts commit e6c5db9d424f778b50d72eb222d734e05967b498.

--- src/main/java/ru/job4j/lombok/Usage.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.lombok;
-
-public class Usage {
-    public static void main(String[] args) {
-        Permission permission = Permission.of().id(1).name("Pavel").rules("all").rules("secret").build();
-        Permission permission1 = Permission.of().id(1).name("Pavel").rules("all").rules("secret").build();
-        System.out.println(permission.equals(permission1));
-    }
-}
